8b16117d61714f796434bc6ee868c3f75a4614f9
Akshay Patil <akshaypatil@Akshays-MacBook-Air.local>
2026-04-08 15:53:44 -0400

    chore(example): add generated refs and versions for built storybooks

--- examples/react/.storybook/refs.generated.ts
+++ examples/react/.storybook/refs.generated.ts
@@ -1,13 +1,19 @@
 export default {
-  "v1.0.0": {
-    "title": "v1.0.0",
-    "url": "/storybooks/1.0.0/",
-    "expanded": false,
-    "disabled": true
-  },
   "v2.0.0": {
     "title": "v2.0.0",
     "url": "/storybooks/2.0.0/",
+    "expanded": false,
+    "disabled": true
+  },
+  "v1.1.0": {
+    "title": "v1.1.0",
+    "url": "/storybooks/1.1.0/",
+    "expanded": false,
+    "disabled": true
+  },
+  "v1.0.0": {
+    "title": "v1.0.0",
+    "url": "/storybooks/1.0.0/",
     "expanded": false,
     "disabled": true
   }
