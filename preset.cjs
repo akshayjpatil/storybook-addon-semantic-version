747dbc7628959a80ea4994b4c8b5cef2661b3d95
Akshay Patil <akshaypatil@Akshays-MacBook-Air.local>
2026-04-16 10:18:51 -0400

    feat: add Storybook 10 support for addon and example

--- preset.cjs
+++
@@ -1,8 +0,0 @@
-'use strict';
-const path = require('path');
-
-const managerEntries = (entry = []) => {
-  return [...entry, path.resolve(__dirname, 'dist/register.js')];
-};
-
-module.exports = { managerEntries };
